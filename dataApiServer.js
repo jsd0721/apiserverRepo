9aca01abe6dfabcaa5d53dc8e23672ebe3e1b987
DESKTOP-G4PJVDG\With_Drone <hiherlin@gmail.com>
2022-01-25 14:41:58 +0900

    update

--- dataApiServer.js
+++ dataApiServer.js
@@ -78,7 +78,7 @@ app.post('/objects/modify',(req,res) =>{
         const rad = req.body.Rad;
         connection.query(`UPDATE objectInfo SET name= '${name}' , lengthX = '${lengthX}', lengthY = '${lengthY}', lengthZ = '${lengthZ}'
             rad = '${rad}' WHERE id='${id}`);
-        res.send("data modified")
+        res.send(result+"data modified");
     } catch (e) {
         console.log(e)
     }
