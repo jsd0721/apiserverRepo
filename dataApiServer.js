0ecdc715aab0b67e8d949694dbbc4a56b7443cb1
CASTLE_D_CHO <whtjdehd12@naver.com>
2022-06-08 15:49:12 +0900

    이메일 인증 콘솔로그 변경

--- dataApiServer.js
+++ dataApiServer.js
@@ -201,7 +201,7 @@ app.post("/join/emailcheck",(req,res)=>{
                 if(err){
                     console.log(err);
                 }else{
-                    console.log('whtjdehd12@naver.com으로 메일 보냄');
+                    console.log(`${req.body.email}으로 메일 보냄`);
                     res.json({
                         log : "email send success",
                         code : 1,
